04d3f1c6292cda0e53cba29622d6afadbefa2e8e
tennisparty <56847529+tennisparty@users.noreply.github.com>
2024-02-28 08:21:02 +0000

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+
+echo '================================================================================'
+echo '|                                                                              |'
+echo '|                   I-Install Software Installation Script                     |'
+echo '|                                                                              |'
+echo '================================================================================'
+
 # Update package lists and upgrade installed packages
+echo '>>> Full upgrade'
 sudo apt update && sudo apt full-upgrade -y
 
 # Set WiFi country to GB (United Kingdom)
+echo '>>> Set WiFi country to GB'
 sudo raspi-config nonint do_wifi_country GB
 
 # Enable I2C interface
+echo '>>> Enable I2C'
 sudo raspi-config nonint do_i2c 0
 
 # Reboot the system
+echo '>>> Starting reboot'
 sudo reboot
